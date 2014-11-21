fdf7a9f9864194158b353fc5ee1b770ba194f11e
Oz Nahum Tiram <nahumoz@gmail.com>
2014-11-21 16:18:20 +0100

    Remove all declarations from main file

--- Makefile
+++ Makefile
@@ -3,15 +3,26 @@ LDFLAGS = -Wl,-rpath,$(LIBROOT)/dataextract
 TDE_LIBS=-L/usr/lib/dataextract -lDataExtract
 TDE_LDFLAGS=-Wl,-rpath=/usr/lib/dataextract
 JSMN=-Ljsmn/ -ljsmn 
+OBJS=mongo.o tde.o json.o
 
-.PHONY: all coll2tde clean
+.PHONY: all clean
+
+all: json tde mongo coll2tde
+
+tde:
+	gcc -c tde.c $(TDE_LIBS) $(TDE_LDFLAGS) 
+
+mongo:
+	gcc -c mongo.c \
+		$(shell pkg-config --cflags --libs libmongoc-1.0) 
+json:
+	gcc -c json.c $(JSMN)
 
 coll2tde:
-	gcc -o coll2tde coll2tde.c \
+	gcc -o coll2tde $(OBJS) coll2tde.c \
 		$(TDE_LIBS) $(TDE_LDFLAGS) $(JSMN) \
 		$(shell pkg-config --cflags --libs libmongoc-1.0) \
 
 clean:
-	rm -v coll2tde
-
+	$(RM) coll2tde $(OBJS)
 
--- coll2tde.c
+++ coll2tde.c
@@ -17,103 +17,6 @@ void print_usage() {
            "COLUMNS -o TDEFILE\n");
 }
 
-#define TryOp(x) if (x != TAB_RESULT_Success) { \
-    fprintf(stderr, "Error: %ls\n", TabGetLastErrorMessage()); \
-    exit( EXIT_FAILURE ); }
-
-/* Define the table's schema */
-TAB_HANDLE MakeTableDefinition()
-{
-    TAB_HANDLE hTableDef;
-    TableauWChar sPurchased[10];
-    TableauWChar sProduct[8];
-    TableauWChar sUProduct[9];
-    TableauWChar sPrice[6];
-    TableauWChar sQuantity[9];
-    TableauWChar sTaxed[6];
-    TableauWChar sExpirationDate[16];
-    TableauWChar sProdukt[8];
-
-    ToTableauString( L"Purchased", sPurchased );
-    ToTableauString( L"Product", sProduct );
-    ToTableauString( L"uProduct", sUProduct );
-    ToTableauString( L"Price", sPrice );
-    ToTableauString( L"Quantity", sQuantity );
-    ToTableauString( L"Taxed", sTaxed );
-    ToTableauString( L"Expiration Date", sExpirationDate );
-    ToTableauString( L"Produkt", sProdukt );
-
-    TryOp( TabTableDefinitionCreate( &hTableDef ) );
-    TryOp( TabTableDefinitionSetDefaultCollation( hTableDef, TAB_COLLATION_en_GB ) );
-    TryOp( TabTableDefinitionAddColumn( hTableDef, sPurchased,      TAB_TYPE_DateTime ) );
-    TryOp( TabTableDefinitionAddColumn( hTableDef, sProduct,        TAB_TYPE_CharString ) );
-    TryOp( TabTableDefinitionAddColumn( hTableDef, sUProduct,       TAB_TYPE_UnicodeString ) );
-    TryOp( TabTableDefinitionAddColumn( hTableDef, sPrice,          TAB_TYPE_Double ) );
-    TryOp( TabTableDefinitionAddColumn( hTableDef, sQuantity,       TAB_TYPE_Integer ) );
-    TryOp( TabTableDefinitionAddColumn( hTableDef, sTaxed,          TAB_TYPE_Boolean ) );
-    TryOp( TabTableDefinitionAddColumn( hTableDef, sExpirationDate, TAB_TYPE_Date ) );
-
-    /* Override Default collation */
-    TryOp( TabTableDefinitionAddColumnWithCollation( hTableDef, sProdukt, TAB_TYPE_CharString, TAB_COLLATION_de ) );
-
-    return hTableDef;
-}
-
-/* Print a Table's schema to stderr */
-void PrintTableDefinition( TAB_HANDLE hTableDef )
-{
-    int i, numColumns, len;
-    TAB_TYPE type;
-    TableauString str;
-    wchar_t* wStr;
-
-    TryOp( TabTableDefinitionGetColumnCount( hTableDef, &numColumns ) );
-    for ( i = 0; i < numColumns; ++i ) {
-        TryOp( TabTableDefinitionGetColumnType( hTableDef, i, &type ) );
-        TryOp( TabTableDefinitionGetColumnName( hTableDef, i, &str ) );
-
-        len = TableauStringLength( str );
-        wStr = malloc( (len + 1) * sizeof(wchar_t) ); /* make room for the null */
-        FromTableauString( str, wStr );
-
-        fprintf(stderr, "Column %d: %ls (%#06x)\n", i, wStr, type);
-        free(wStr);
-    }
-}
-
-/* Insert a few rows of data. */
-void InsertData( TAB_HANDLE hTable )
-{
-    TAB_HANDLE hRow;
-    TAB_HANDLE hTableDef;
-    int i;
-
-    TableauWChar sUniBeans[9];
-    ToTableauString( L"uniBeans", sUniBeans );
-
-    TryOp( TabTableGetTableDefinition( hTable, &hTableDef ) );
-
-   /* Create a row to insert data */
-    TryOp( TabRowCreate( &hRow, hTableDef ) );
-
-    TryOp( TabRowSetDateTime(   hRow, 0, 2012, 7, 3, 11, 40, 12, 4550 ) ); /* Purchased */
-    TryOp( TabRowSetCharString( hRow, 1, "Beans" )  );                     /* Product */
-    TryOp( TabRowSetString(     hRow, 2, sUniBeans ) );                    /* uProduct */
-    TryOp( TabRowSetDouble(     hRow, 3, 1.08 ) );                         /* Price */
-    TryOp( TabRowSetDate(       hRow, 6, 2029, 1, 1 ) );                   /* Expiration date */
-    TryOp( TabRowSetCharString( hRow, 7, "Bohnen" ) )                      /* Produkt */
-
-    /* Insert a few rows */
-    for ( i = 0; i < 10; ++i ) {
-        TryOp( TabRowSetInteger(  hRow, 4, i * 10) );                      /* Quantity */
-        TryOp( TabRowSetBoolean(  hRow, 5, i % 2 ) );                      /* Taxed */
-        TryOp( TabTableInsert( hTable, hRow ) );
-    }
-
-    TryOp( TabRowClose( hRow ) );
-    TryOp( TabTableDefinitionClose( hTableDef ) );
-}
-
 /* Wrap all mongodb stuff in a function */
 mongoc_cursor_t *
 get_cursor(char *host, char *db, char *collection_name, bson_t *pquery,
@@ -201,43 +104,7 @@ main (int   argc, char *argv[]){
    
 
     printf("filename: %s\n", filename);
-# if 0
-    TAB_HANDLE hExtract;
-    TAB_HANDLE hTableDef;
-    TAB_HANDLE hTable;
-    int i;
-    int bHasTable;
-
-    TableauWChar sOrderTde[12];
-    TableauWChar sExtract[8];
-    ToTableauString( L"order-c.tde", sOrderTde );
-    ToTableauString( L"Extract", sExtract );
     
-    TryOp( TabExtractCreate( &hExtract, sOrderTde ) );
-    TryOp( TabExtractHasTable( hExtract, sExtract, &bHasTable ) );
-
-    if ( !bHasTable ) {
-        /* Table does not exist; create it. */
-        hTableDef = MakeTableDefinition();
-        TryOp( TabExtractAddTable( hExtract, sExtract, hTableDef, &hTable ) );
-        TryOp( TabTableDefinitionClose( hTableDef ) );
-    }
-    else {
-        /* Open an existing table to add more rows. */
-        TryOp( TabExtractOpenTable( hExtract, sExtract, &hTable ) );
-    }
-
-    TryOp( TabTableGetTableDefinition( hTable, &hTableDef ) );
-    PrintTableDefinition( hTableDef );
- 
-    InsertData( hTable );
-
-    /* Clean up */
-    TryOp( TabTableDefinitionClose( hTableDef ) );
-    TryOp( TabExtractClose( hExtract ) );
-
-    printf ("done with tableu kram\n");
-# endif 
     mongoc_client_t *client_p;
     mongoc_collection_t *collection_p;
     mongoc_cursor_t *cursor;
@@ -245,7 +112,7 @@ main (int   argc, char *argv[]){
     bson_t *query = NULL;
     char *str;
     int t, r;
-    jsmntok_t tokens[9];
+    jsmntok_t tokens[13];
     jsmn_parser parser;
     jsmn_init(&parser);
     mongoc_init();
@@ -256,7 +123,7 @@ main (int   argc, char *argv[]){
     while (mongoc_cursor_next (cursor, &doc)) {
         str = bson_as_json (doc, NULL);
         printf ("%s\n", str);
-	    r = jsmn_parse(&parser, str, strlen(str), tokens, 9);
+	    r = jsmn_parse(&parser, str, strlen(str), tokens, 13);
         printf("tokens: %d\n", r - 1);
         int skip;
         for (t=0; t<r; t++){
