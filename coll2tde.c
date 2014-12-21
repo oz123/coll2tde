e66be74d413afe3a4b628b0f1b7372fb99e39472
Oz Nahum Tiram <nahumoz@gmail.com>
2014-12-21 03:34:48 +0100

    Code clean
     * move option parsing to own function
     * make query and aggregation mutually exclusive

--- coll2tde.c
+++ coll2tde.c
@@ -5,19 +5,63 @@
 #include "tde.h"
 
 #define FIELDS 999 
-#define AGGR 1001 
+#define HELP 998 
 
-//static int verbose_flag;
 
 void print_usage() {
     printf("Usage: coll2tde -h HOST -d DATABASE -c COLLECTION [-q QUERY][--fields "
            "FIELDS] [-a AGGREGATION] -f TDEFILE\n");
 }
 
+void get_options(int argc, char *argv[], char **host, char **database,
+    char **collection_name, char **filename, char **fields, char **query, 
+    char **aggregation) {
+
+    int opt = 0;
+    int long_index = 0;
+    
+    static struct option long_options[] = {
+        {"host",      required_argument,       0,  'h' },
+        {"database", required_argument,       0,  'd' },
+        {"collection",    required_argument, 0,  'c' },
+        {"fields",   required_argument, 0,  COLS },
+        {"query",   required_argument, 0,  'q' },
+        {"aggregation",   required_argument, 0,  'a' },
+        {"filename",   required_argument, 0,  'o' },
+        {"help",   no_argument, 0,  HELP },
+        {0,           0,                 0,  0   }
+    };
+
+    while ((opt = getopt_long(argc, argv,"h:d:c:f:q:a:", 
+                   long_options, &long_index )) != -1) {
+        switch (opt) {
+             
+            case 'h' : *host = optarg;
+                 break;
+             case 'd' : *database = optarg;
+                 break;
+             case 'c' : *collection_name = optarg; 
+                 break;
+             case 'f' : *filename = optarg;
+                 break;
+             case 'q' : *query = optarg;
+                 break;
+             case FIELDS: *fields = optarg;
+                 break;
+             case 'a' : *aggregation = optarg;
+                 break;
+             default: print_usage(); 
+                 exit(EXIT_FAILURE);
+        }
+
+
+    }
+}
 
 int
-main (int   argc, char *argv[]){
-    int opt = 0;
+main (int argc, char *argv[]){
+    
+
     char *host = NULL;
     char *database = NULL;
     char *collection_name = NULL;
@@ -26,62 +70,27 @@ main (int   argc, char *argv[]){
     char *query = NULL;
     char *aggregation = NULL;
     
-    static struct option long_options[] = {
-        {"host",      required_argument,       0,  'h' },
-        {"database", no_argument,       0,  'd' },
-        {"collection",    required_argument, 0,  'c' },
-        {"fields",   required_argument, 0,  COLS },
-        {"query",   required_argument, 0,  'q' },
-        {"aggregation",   required_argument, 0,  AGGR },
-        {"filename",   required_argument, 0,  'o' },
-        {0,           0,                 0,  0   }
-    };
-
-    int long_index =0;
-    while ((opt = getopt_long(argc, argv,"h:d:c:f:q:", 
-                   long_options, &long_index )) != -1) {
-        switch (opt) {
-             case 'h' : host = optarg;
-                 break;
-             case 'd' : database = optarg;
-                 break;
-             case 'c' : collection_name = optarg; 
-                 break;
-             case 'f' : filename = optarg;
-                 break;
-             case 'q' : query = optarg;
-                 break;
-             case FIELDS: fields = optarg;
-                 break;
-             case AGGR : aggregation = optarg;
-                 break;
-             default: print_usage(); 
-                 exit(EXIT_FAILURE);
-        }
-
-
-    }
-    /* if no file name is given the collection_name will be appended tde */
-    printf("host: %s\n", host);
-    printf("database: %s\n", database);
-    printf("collection_name: %s\n", collection_name);
+    get_options(argc, argv, &host, &database, &collection_name, &filename, 
+            &fields, &query, &aggregation);
 
     if ( host == NULL || database == NULL || collection_name == NULL)  {
         print_usage();
         exit(EXIT_FAILURE);
     }
     
+    if ( query && aggregation ) {
+        printf("Aggregation and query are mutually exclusive...\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    /* if no file name is given the collection_name will be appended tde */
     if (filename == NULL) {
         filename = malloc(strlen(collection_name));
         strcpy(filename, collection_name);
         strcat(filename, ".tde");  
     }
+
     
-
-    if ( aggregation != NULL ) {
-        printf("aggregation %s\n", aggregation);
-    }
-
     mongoc_client_t *client_p;
     mongoc_collection_t *collection_p;
     mongoc_cursor_t *cursor = NULL;
@@ -90,28 +99,26 @@ main (int   argc, char *argv[]){
     TAB_HANDLE hTable;
     TAB_HANDLE hTableDef;
     wchar_t *fname_w = calloc(strlen(filename) + 1, sizeof(wchar_t));
+    char *jsstr = NULL;
+    TAB_TYPE *column_types = NULL;
+    int ncol = 0;
+    int r = 0;
+    int bHasTable;
+    
     mbstowcs(fname_w, filename, strlen(filename)+1);
     TableauWChar *sfname = calloc(strlen(filename) + 1, sizeof(TableauWChar));
     TableauWChar sExtract[8];
     ToTableauString(fname_w, sfname);
     ToTableauString(L"Extract", sExtract);
     TryOp(TabExtractCreate(&hExtract, sfname));
-    char *jsstr = NULL;
-    /* TODO: add option to parse aggregation, and get this cursor 
-     * aggregation should be given as json and converted to BSON with 
-     * bson_new_from_json
-     * mongoc_collection_aggregate should than be used to get a cursor
-     * aggregation_json should begin with pipeline and contain array of
+
+     /* aggregation_json should begin with pipeline and contain array of
      * operations:
      * { "pipeline" : [ { "$project" : { "name" : 1 } }, { "$group" : 
      *                  { "_id" : "$name", "credit" : { "$sum" : 1 } } }
      *                ] 
      *  }
      * */
-    TAB_TYPE *column_types = NULL;
-    int ncol = 0;
-    int r = 0;
-    int bHasTable;
     TryOp(TabExtractHasTable(hExtract, sExtract, &bHasTable));
 
     cursor = get_cursor(host, database, collection_name, query, fields,  
@@ -151,7 +158,6 @@ main (int   argc, char *argv[]){
     }
     
     printf("Successfully inserted %d rows\n", r);
-    
     TryOp(TabExtractClose(hExtract));
     mongoc_cursor_destroy(cursor);
     mongoc_collection_destroy(collection_p);
