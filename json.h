c94cb60c71269ac7e97a27ef6bd178ef3419879e
Oz Nahum Tiram <nahumoz@gmail.com>
2014-12-27 22:22:54 +0100

    Add copyright notice

--- json.h
+++ json.h
@@ -1,11 +1,36 @@
+/* 
+ * This file is part of coll2tde.
+ *
+ * coll2tde is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3
+ * as published by the Free Software Foundation;
+ *
+ * coll2tde is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with coll2tde; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * ============================================================================
+ * Copyright (C) 2014 Oz Nahum Tiram <nahumoz@gmail.com>
+ * ============================================================================
+ */
+
+/* JSON parsers and helpers */
+
+#pragma once
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include "jsmn/jsmn.h"
 
-/* JSON parsers and helpers */
 jsmntok_t * json_tokenise(char *js);
-bool json_token_streq(char *js, jsmntok_t *t, char *s);
-char * json_token_tostr(char *js, jsmntok_t *t);
 
+bool json_token_streq(char *js, jsmntok_t *t, char *s);
+
+char * json_token_tostr(char *js, jsmntok_t *t);
--- mongo.h
+++ mongo.h
@@ -1,3 +1,26 @@
+/* 
+ * This file is part of coll2tde.
+ *
+ * coll2tde is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3
+ * as published by the Free Software Foundation;
+ *
+ * coll2tde is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with coll2tde; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * ============================================================================
+ * Copyright (C) 2014 Oz Nahum Tiram <nahumoz@gmail.com>
+ * ============================================================================
+ */
+
+#pragma once
+
 #include <mongoc.h>
 #include "log.h"
 
