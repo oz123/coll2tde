b55d3772e5d10c65e24b48978c0fa3b3243b6242
Oz Nahum Tiram <nahumoz@gmail.com>
2015-02-17 23:03:23 +0100

    Some more BSON types

--- example1.c
+++ example1.c
@@ -15,41 +15,40 @@ main (int   argc,
 
     bson_t *query;
     char *str;
-    /* bson_iter_t iter - is meant to be used on the stack and can be 
+    /* bson_iter_t iter - is meant to be used on the stack and can be
      * discarded at any time as it contains no external allocation.
      */
     bson_iter_t iter;
-    //bson_type_t type; // we can use that, but it's quite redundant. 
+    //bson_type_t type; // we can use that, but it's quite redundant.
     mongoc_init ();
 
     client = mongoc_client_new ("mongodb://localhost:27017/");
     collection = mongoc_client_get_collection (client, "test", "test");
     query = bson_new ();
-    cursor = mongoc_collection_find (collection, MONGOC_QUERY_NONE, 0, 
+    cursor = mongoc_collection_find (collection, MONGOC_QUERY_NONE, 0,
                                      0, 0, query, NULL, NULL);
 
     while (mongoc_cursor_next (cursor, &doc)) {
-            str = bson_as_json (doc, NULL);
-            if (bson_iter_init (&iter, doc)) {
-               while (bson_iter_next (&iter)) {
-                     printf ("Found element key: \"%s\"\n", 
-                             bson_iter_key (&iter));
-                        //type = bson_iter_type (&iter);
-                        //printf("type %d\n", (int)type);
-                        value = bson_iter_value(&iter);
-                        printf("Found values of type %d " , 
-                               (*value).value_type);
-                        if ((*value).value_type == BSON_TYPE_UTF8) {
-                            printf("(UTF-8)\nThe value is %s\n",  
-                                   (*value).value.v_utf8.str);
-                        }
-                     }
+        str = bson_as_json (doc, NULL);
+        if (bson_iter_init (&iter, doc)) {
+            while (bson_iter_next (&iter)) {
+                printf ("Found element key: \"%s\"\n", bson_iter_key (&iter));
+                value = bson_iter_value(&iter);
+                if ((*value).value_type == BSON_TYPE_UTF8) {
+                    printf("(UTF-8)\nThe value is %s\n", (*value).value.v_utf8.str);
+                } else if ((*value).value_type == BSON_TYPE_DOCUMENT ||
+                            (*value).value_type == BSON_TYPE_ARRAY) {
+                    /* we can properly detect array and documents too ...*/
+                    printf("buah! exploding with loud crash, a table can't tollerate document or array as a value!\n");
+                } else if ((*value).value_type == BSON_TYPE_OID) {
+                /* ignore objectid */
+                printf("dying silently on oid...\n");
+                }
             }
-            bson_free(str);
-            
-
         }
-    
+        bson_free(str);
+    }
+
     bson_destroy (query);
     mongoc_cursor_destroy (cursor);
     mongoc_collection_destroy (collection);
