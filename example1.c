b65f681088a238c5ed14cbd5f22726f1d1f4cc01
Oz Nahum Tiram <nahumoz@gmail.com>
2015-02-17 21:35:37 +0100

    We now have some sense how to work with libbson

--- example1.c
+++ example1.c
@@ -7,36 +7,49 @@ int
 main (int   argc,
               char *argv[])
 {
+    const bson_t *doc;
+    const bson_value_t *value;
     mongoc_client_t *client;
     mongoc_collection_t *collection;
     mongoc_cursor_t *cursor;
-    const bson_t *doc;
-    const bson_value_t *value;
+
     bson_t *query;
     char *str;
+    /* bson_iter_t iter - is meant to be used on the stack and can be 
+     * discarded at any time as it contains no external allocation.
+     */
     bson_iter_t iter;
-    bson_type_t type;
+    //bson_type_t type; // we can use that, but it's quite redundant. 
     mongoc_init ();
 
     client = mongoc_client_new ("mongodb://localhost:27017/");
     collection = mongoc_client_get_collection (client, "test", "test");
     query = bson_new ();
-    cursor = mongoc_collection_find (collection, MONGOC_QUERY_NONE, 0, 0, 0, query, NULL, NULL);
+    cursor = mongoc_collection_find (collection, MONGOC_QUERY_NONE, 0, 
+                                     0, 0, query, NULL, NULL);
 
     while (mongoc_cursor_next (cursor, &doc)) {
             str = bson_as_json (doc, NULL);
             if (bson_iter_init (&iter, doc)) {
                while (bson_iter_next (&iter)) {
-                     printf ("Found element key: \"%s\"\n", bson_iter_key (&iter));
-                        type = bson_iter_type (&iter);
-                        printf("type %d\n", (int)type);
-                        value = bson_iter_value (&iter);
-                        printf("Found values of type %d\n", (*value).value_type);
+                     printf ("Found element key: \"%s\"\n", 
+                             bson_iter_key (&iter));
+                        //type = bson_iter_type (&iter);
+                        //printf("type %d\n", (int)type);
+                        value = bson_iter_value(&iter);
+                        printf("Found values of type %d " , 
+                               (*value).value_type);
+                        if ((*value).value_type == BSON_TYPE_UTF8) {
+                            printf("(UTF-8)\nThe value is %s\n",  
+                                   (*value).value.v_utf8.str);
+                        }
                      }
             }
             bson_free(str);
-        }
+            
 
+        }
+    
     bson_destroy (query);
     mongoc_cursor_destroy (cursor);
     mongoc_collection_destroy (collection);
